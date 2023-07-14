51d04c0d483d311fe55ea8d17dec1d698d5a4089
alessiovietri <info@alessiovietri.it>
2023-07-14 12:55:12 +0200

    Completato LC

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,40 +1,170 @@
 <script>
+import SingleProduct from './SingleProduct.vue';
+
 export default {
     name: "MainComponent",
     data() {
         return {
-
-        }
+            products: [
+                {
+                    src: "https://www.lamolisana.it/wp-content/uploads/2021/04/1-spaghetto-quadrato.jpg",
+                    titolo: "N.4 Spaghetto Quadrato Bucato",
+                    tipo: "lunga",
+                    cottura: 6,
+                },
+                {
+                    src: "https://www.lamolisana.it/wp-content/uploads/2021/04/5-fettuccine.jpg",
+                    titolo: "N.5 Fettuccine",
+                    tipo: "lunga",
+                    cottura: 13,
+                },
+                {
+                    src: "https://www.lamolisana.it/wp-content/uploads/2021/04/6-linguine.jpg",
+                    titolo: "N.6 Linguine",
+                    tipo: "lunga",
+                    cottura: 12,
+                },
+                {
+                    src: "https://www.lamolisana.it/wp-content/uploads/2021/04/7-mezze-linguine.jpg",
+                    titolo: "N.7 Mezze Linguine",
+                    tipo: "lunga",
+                    cottura: 11,
+                },
+                {
+                    src: "https://www.lamolisana.it/wp-content/uploads/2021/04/11-spaghettino-quadrato.jpg",
+                    titolo: "n.11 Spaghettino Quadrato",
+                    tipo: "lunga",
+                    cottura: 9,
+                },
+                {
+                    src: "https://www.lamolisana.it/wp-content/uploads/2021/05/38-mezze-maniche.jpg",
+                    titolo: "N.38 Mezze maniche",
+                    tipo: "corta",
+                    cottura: 12,
+                },
+                {
+                    src: "https://www.lamolisana.it/wp-content/uploads/2021/05/44-sedani.jpg",
+                    titolo: "N.44 Sedani",
+                    tipo: "corta",
+                    cottura: 8,
+                },
+                {
+                    src: "https://www.lamolisana.it/wp-content/uploads/2021/05/49-sedani.jpg",
+                    titolo: "N.49 Sedanini",
+                    tipo: "corta",
+                    cottura: 8,
+                },
+                {
+                    src: "https://www.lamolisana.it/wp-content/uploads/2021/05/53-lumachine.jpg",
+                    titolo: "N.53 Lumachine",
+                    tipo: "corta",
+                    cottura: 12,
+                },
+                {
+                    src: "https://www.lamolisana.it/wp-content/uploads/2021/05/55-chifferi-rigati.jpg",
+                    titolo: "N.55 Chifferi rigati",
+                    tipo: "corta",
+                    cottura: 12,
+                },
+                {
+                    src: "https://www.lamolisana.it/wp-content/uploads/2021/05/58-misto-corto.jpg",
+                    titolo: "N.58 Misto corto",
+                    tipo: "corta",
+                    cottura: 9,
+                },
+                {
+                    src: "https://www.lamolisana.it/wp-content/uploads/2021/06/84-ditale-quadrato.jpg",
+                    titolo: "N.84 Ditale Quadrato",
+                    tipo: "cortissima",
+                    cottura: 12,
+                },
+            ],
+            onSale: [
+                {
+                    src: "https://www.lamolisana.it/wp-content/uploads/2021/05/58-misto-corto.jpg",
+                    titolo: "N.58 Misto corto",
+                    tipo: "corta",
+                    cottura: 9,
+                },
+                {
+                    src: "https://www.lamolisana.it/wp-content/uploads/2021/06/84-ditale-quadrato.jpg",
+                    titolo: "N.84 Ditale Quadrato",
+                    tipo: "cortissima",
+                    cottura: 12,
+                },
+            ],
+        };
     },
-    methods: {
-
-    }
-}
+    components: {
+        SingleProduct
+    },
+};
 </script>
 
 <template>
     <main>
-		<div class="container">
-			<h1>
-				--] Content goes here [--
-			</h1>
-		</div>
+        <div class="container">
+            <h1>Lista prodotti</h1>
+
+            <div class="products">
+                <SingleProduct class="product"
+                    v-for="(product, productIndex) in products" :key="productIndex"
+                    :productImage="product.src"
+                    :productTitle="product.titolo" />
+            </div>
+
+            <h3>Prodotti in offerta</h3>
+
+            <div class="products">
+                <SingleProduct class="product on-sale"
+                    v-for="(product, productIndex) in onSale" :key="productIndex"
+                    :productImage="product.src"
+                    :productTitle="product.titolo" />
+            </div>
+        </div>
     </main>
 </template>
 
 <style lang="scss" scoped>
 @use "../assets/scss/partials/layout" as *;
 
-main {
-	padding: 80px 0;
-	background-image: url('../assets/img/fondo-pag-speciali.jpg');
-	background-position: center;
-	background-size: cover;
-	background-repeat: no-repeat;
+$main-color: blue;
 
-	h1 {
-		color: blue;
-		text-align: center;
-	}
+main {
+    padding: 80px 0;
+    background-image: url("../assets/img/fondo-pag-speciali.jpg");
+    background-position: center;
+    background-size: cover;
+    background-repeat: no-repeat;
+
+    h1, h3 {
+        color: $main-color;
+        text-align: center;
+    }
+
+    h3 {
+        margin-top: 60px;
+    }
+
+    .products {
+        display: flex;
+        flex-wrap: wrap;
+
+        .product {
+            background-color: white;
+            width: 25%;
+            border: 1px solid cadetblue;
+
+            &.on-sale {
+                width: 50%;
+            }
+
+            > h2 {
+                color: $main-color;
+                text-align: center;
+                padding: 20px 10px;
+            }
+        }
+    }
 }
 </style>
---
+++ src/components/SingleProduct.vue
@@ -0,0 +1,35 @@
+<script>
+export default {
+    data() {
+        return {
+            // product: {
+            //     src: "https://www.lamolisana.it/wp-content/uploads/2021/06/84-ditale-quadrato.jpg",
+            //     titolo: "N.84 Ditale Quadrato",
+            //     tipo: "cortissima",
+            //     cottura: 12,
+            // }
+            // productImage: 'https://www.lamolisana.it/wp-content/uploads/2021/06/84-ditale-quadrato.jpg',
+            // productTitle: 'Pasta 1',
+        };
+    },
+    props: {
+        productImage: String,
+        productTitle: String
+    }
+};
+</script>
+
+<template>
+    <div class="single-product">
+        <div>
+            <img
+                :src="productImage"
+                :alt="productTitle"
+            />
+        </div>
+        <h2>{{ productTitle }}</h2>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+</style>
